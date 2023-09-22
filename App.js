f5b322d02885064f0604647fd94dcef57937e26b
haris-shahbaz <harisshahbaz707@gmail.com>
2023-09-22 00:37:52 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,111 @@
+// Chapter no 17 to 20
+
+
+// Question no 1 and 2
+// var arry = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ];
+// console.log (arry);
+
+
+// Question no 3
+// for (var i = 1; i <= 10; i++) {
+//     console.log (i);
+// }
+
+
+// Question no 4
+// var table = prompt("Table Number");
+// var length = prompt("Table Length");
+// for (var i = 1; i <= length; i++) {
+//     console.log (table * i);
+// }
+
+
+// Question no 5
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// var length = fruits.length;
+// for (var i = 0; i <= length; i++) {
+//    console.log (fruits[i]);
+// }    
+
+
+// Question no 6
+// for (var i = 1; i <= 15; i++) {
+//     console.log (i);
+// }
+// for (var i = 10; i > 0; i--) {
+//     console.log (i);
+// }
+// var evens = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+// var length = evens.length;
+// for (var i = 0; i <= length; i++) {
+//     console.log (evens[i]);
+// }
+// var odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+// var length = odds.length;
+// for (var i = 0; i <= length; i++) {
+//     console.log (odds[i]);
+// }
+// var series = ["2K", "4K", "6K", "8K", "10K", "12K", "14K", "16K", "18K", "20K"];
+// var length = series.length;
+// for (var i = 0; i <= length; i++) {
+//     console.log (series[i]);
+// }
+
+
+// Question no 7
+// const A = ["cake", "apple pie", "cookie", "chips", "patties"];
+// function searchItem(item) {
+//   item = item.toLowerCase();
+//   if (A.includes(item)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// const userInput = prompt("Enter an item to search for:");
+// if (userInput !== null) {
+//   const found = searchItem(userInput.toLowerCase());
+
+//   if (found) {
+//     alert("The item is found in the list.");
+//   } else {
+//     alert("The item is not found in the list.");
+//   }
+// } else {
+//   alert("User canceled the search.");
+// }
+
+
+// Question no 8
+// var A = [24, 53, 78, 91, 12];
+// var largest = A[0];
+// for (var i = 1; i < A.length; i++) {
+//     if (A[i] > largest) {
+//         largest = A[i];
+//     }
+// }
+// console.log (largest);
+
+
+// Question no 9
+// var A = [24, 53, 78, 91, 12];
+// var smallest = A[0];
+// for (var i = 1; i < A.length; i++) {
+//     if (A[i] < smallest) {
+//         smallest = A[i];
+//     }
+// }
+// console.log (smallest);
+
+
+// Question no 10
+// var num = 5;
+// for (var i = 1; i <= 20; i++) {
+//     console.log (i * num);
+// }
+
+
